427ea0466d95454a847999e9c0f4a991de40c078
Ki Rill <rill.ki@yahoo.com>
2025-04-14 22:12:09 +0500

    update

--- source/app.d
+++ source/app.d
@@ -4,60 +4,118 @@ import std.csv;
 import std.conv;
 import std.file;
 import std.stdio;
+import std.regex;
+import std.array;
+import std.random;
 import std.string;
 import std.datetime;
+import std.algorithm;
+
+struct MyDate
+{
+    Date date;
+    alias this = date;
+    this(in string d)
+    {
+        this.date = Date.fromISOExtString(d);
+    }
+}
 
 struct Layout
 {
-	Date date;
-	float price, open, high, low;
-	string vol, change;
+    MyDate date;
+    float open, high, low, close;
 }
 
-string sanitizeCsv(string csv) {
-    import std.regex : regex, replaceAll;
-
-    // this regex finds numbers like "4,166.82" and removes the comma inside quotes
-    auto numberWithCommas = regex(`"(\d{1,3}),(\d{3}\.\d+)"`);
-    return csv
-		.replaceAll(numberWithCommas, `"$1$2"`)
-		.replace("Date", "date")
-		.replace("Price", "price")
-		.replace("Open", "open")
-		.replace("High", "high")
-		.replace("Low", "low")
-		.replace("Vol.", "vol")
-		.replace("Change %", "change")
-		.replace("Date", "date")
-		.replace("\"", "");
+enum BetType: int 
+{
+    no = 0,
+    yes = 1,
+    random = -1,
 }
 
+struct Context
+{
+    float balance, payout;
+    int betAmount;
+    BetType betAction;
+}
+
+// WIP
 void main(string[] args)
 {
-	if (args.length < 2)
-	{
-		writeln("USAGE: ./binary_options CSVFILE");
-		return;
-	}
+    if (args.length < 2)
+    {
+        writeln("USAGE: ./binary_options file.csv");
+        return;
+    }
 
-	auto data = readText(args[1]).sanitizeCsv;
-	Layout[] df;
-	foreach (i, line; data.splitLines)
-	{
-		if (!i) continue;
-		auto p = line.split(",");
-		auto d = p[0].split("/");
-		df ~= Layout(
-			Date(d[2].to!int, d[0].to!int, d[1].to!int),
-			p[1].to!float,
-			p[2].to!float,
-			p[3].to!float,
-			p[4].to!float,
-			p[5],
-			p[6],
-		);
-	}
-	df.writeln;
-	// auto df = csvReader!Layout(data, ',', '"', true);
-	// writeln(df);
+    // process csv
+    auto df = args[1]
+        .readText
+        .csvReader!Layout(["Date", "Open", "High", "Low", "Close"])
+        .array;
+    
+    // init
+    auto context = Context(
+        balance: 100,                   // initial balance
+        payout: 1,                      // percentage payout (1 == 100%)
+        betAmount: 1,                   // bet size
+        betAction: BetType.yes,      // start buy randomly choosing yes/no
+    );
+
+    // run backtest
+    float netProfit = 0;
+    int n_yes, n_no, n_wins, n_looses;
+    auto prev = df[0];
+    foreach (i, current; df)
+    {
+        if (!i) continue;               // skip the first row just to have something before the start of simulation
+        if (context.balance <= 0) break;// stop trading once the account is blown up
+
+        // bet action
+        auto action = context.betAction == BetType.random ? uniform!"[]"(0, 1).to!BetType : context.betAction;
+        auto priceDiff = current.open - prev.close;
+        if (action == BetType.yes)
+        {
+            n_yes++;
+            if (priceDiff > 0)
+            {
+                n_wins++;
+                context.balance += context.payout * context.betAmount;
+                netProfit += context.payout * context.betAmount;
+            }
+            else 
+            {
+                n_looses++;
+                context.balance -= context.betAmount;
+                netProfit -= context.betAmount;
+            }
+        }
+        else
+        {
+            n_no++;
+            if (priceDiff < 0)
+            {
+                n_wins++;
+                context.balance += context.payout * context.betAmount;
+                netProfit += context.payout * context.betAmount;
+            }
+            else 
+            {
+                n_looses++;
+                context.balance -= context.betAmount;
+                netProfit -= context.betAmount;
+            }
+        }
+
+        // log
+        writefln(
+            "%12s ==> balance: %5s | net profit: %5s | n_wins: %5s | n_losses: %5s", 
+            current.date, context.balance, netProfit, n_wins, n_looses,
+        );
+
+        prev = current;
+    }
 }
+
